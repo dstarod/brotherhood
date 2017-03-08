ea8597c6cb5e4454460262232e75855da1b18958
DMITRY STARODUBTSEV <dmitry.starodubcev@gmail.com>
2017-03-09 00:02:53 +0300

    [add] Initial commit: add README and basis

---
+++ brotherhood/settings.py
@@ -0,0 +1,93 @@
+import os
+from .private import (
+    DEBUG, SECRET_KEY, DB_NAME, DB_HOST, DB_PASS, DB_PORT, DB_USER
+)
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'bootstrapform',
+    'bh'
+]
+
+MIDDLEWARE_CLASSES = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'brotherhood.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        ,
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'brotherhood.wsgi.application'
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASS,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT
+    },
+}
+
+# Password validation
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+TIME_ZONE = 'UTC'
+USE_TZ = False
+
+LANGUAGE_CODE = 'ru-ru'
+USE_I18N = True
+USE_L10N = True
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
---
+++ brotherhood/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.conf import settings
+from django.views import static
+
+
+urlpatterns = [
+    url(r'', include('bh.urls', namespace='bh')),
+    url(r'^admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns.append(
+        url(
+            r'^media/(?P<path>.*)$',
+            static.serve,
+            {'document_root': settings.MEDIA_ROOT}
+        )
+    )
